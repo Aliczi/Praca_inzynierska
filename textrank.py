43fb444be53122b5f39ddaec1daf9dc1f3250062
panka134 <134panka@gmail.com>
2022-12-04 22:08:29 +0100

    Split to files and clean

--- textrank.py
+++ textrank.py
@@ -1,8 +1,8 @@
-from datasets import load_dataset
 import pandas as pd
 from noun_chunks_pl import noun_chunks_pl
 import spacy
 import pytextrank
+from tools import load_data, save_dicts_to_files
 
 # dependencies to_ install
 # !pip install pytextrank
@@ -11,9 +11,11 @@ import pytextrank
 
 class TextRank:
     def __init__(self, language_model: str):
-        self.nlp = spacy.load(language_model) 
-        spacy.lang.pl.PolishDefaults.syntax_iterators = {"noun_chunks": self._get_chunks}  # noun_chunk replacement for polish
-        self.nlp = spacy.load(language_model)  # Create language model
+        self.nlp = spacy.load(language_model)
+        spacy.lang.pl.PolishDefaults.syntax_iterators = {
+            "noun_chunks": self._get_chunks
+        }  # noun_chunk replacement for polish
+        self.nlp = spacy.load(language_model)
 
         # IDEA: add "hotel to stopwords"
         self.nlp.add_pipe("textrank")
@@ -22,12 +24,14 @@ class TextRank:
         return noun_chunks_pl(doc)
 
     def _lemmatize(self, text: str) -> str:
-        """ Return lemmatized text"""
+        """Return lemmatized text"""
         doc = self.nlp(text)
         return " ".join([token.lemma_ for token in doc])
 
-    def get_keywords(self, text: str, len=None, lemmatized=False) -> list:
-        """ Get keywords from text """
+    def _get_keywords(
+        self, text: str, len=None, lemmatized=False
+    ) -> list:
+        """Get keywords from text"""
         if lemmatized:
             text = self._lemmatize(text)
 
@@ -37,53 +41,42 @@ class TextRank:
             keywords = keywords[:len]
         return keywords
 
-    def create_dicts_for_all_classes(self, df: pd.DataFrame, target_colname="target", opinion_colname="text", len=25, trainset_size=100) -> dict:
-        """ Create dictionaries with keywords for every oppinion class in df. 
-            Returns a dictionary in form {class0: [keyword0, keyword1, ...], ...} """
+    def create_dicts_for_all_classes(
+        self,
+        df: pd.DataFrame,
+        target_colname: str = "target",
+        opinion_colname: str = "text",
+        len: int = 25,
+        trainset_size: int = 100,
+    ) -> dict:
+        """Create dictionaries with keywords for every oppinion class in df.
+        Returns a dictionary in form {class0: [keyword0, keyword1, ...], ...}"""
         keywords = dict()
+
+        df_filtered = df[:trainset_size]  # TODO: DELETE THIS!
+        df_filtered = df_filtered.groupby(target_colname).sum(opinion_colname)
+        keywords = dict(df_filtered[opinion_colname].apply(self._get_keywords, len=len))
         
-        for sentiment in df[target_colname].unique():
-            # filter opinions by sentiment
-            df_filtered = df[df[target_colname] == sentiment]
-            df_filtered.drop(target_colname, axis=1)
-
-            # get keywords
-            df_filtered = df_filtered[:trainset_size]  # TODO: DELETE THIS!
-            all_oppinions = df_filtered[opinion_colname].sum()  # TODO: too long
-            keywords[sentiment] = self.get_keywords(all_oppinions, len=len)
-
         return keywords
 
-def save_dicts_to_files(dicts: dict):
-    for sentiment in dicts.keys():
-        filename = f"textrank_{sentiment}.txt"
-        with open(filename, 'w') as file:
-            file.write('\n'.join(dicts[sentiment]))
-        print(f"Keywords for sentiment {sentiment} written to file {filename}")
 
 
-if __name__ == '__main__':
-    # ---------------------------------
-    # ADJUST PARAMETERS
-
-    polemo_category = "hotels_text"  # only opinions about hotels
-    # available categories: 'all_text', 'all_sentence', 'hotels_text', 'hotels_sentence', 'medicine_text', 'medicine_sentence',
-    # 'products_text', 'products_sentence', 'reviews_text', 'reviews_sentence'
-
+if __name__ == "__main__":
+    # --------------------------------------------------------------------------
     number_of_keywords = 100
-    number_of_opinions = 50
-    # ----------------------------------
+    number_of_opinions = 100
+    polemo_category = "hotels_text"  # only opinions about hotels
+    # available categories: 'all_text', 'all_sentence',
+    # 'hotels_text', 'hotels_sentence', 'medicine_text', 'medicine_sentence',
+    # 'products_text', 'products_sentence', 'reviews_text', 'reviews_sentence'
+    # --------------------------------------------------------------------------
 
-    # read data from polemo
-    polemo_official = load_dataset("data/polemo2-official/", polemo_category)
-    df_polemo_official = pd.DataFrame(polemo_official["train"])
-
-    # create dictionaries
+    df_polemo_official = load_data("data/polemo2-official/", polemo_category)
     textRank = TextRank("pl_core_news_sm")
-    dicts = textRank.create_dicts_for_all_classes(df_polemo_official, len=number_of_keywords, trainset_size=number_of_opinions)
+    dicts = textRank.create_dicts_for_all_classes(
+        df_polemo_official,
+        len=number_of_keywords,
+        trainset_size=number_of_opinions
+    )
 
-    # save them
-    save_dicts_to_files(dicts)
-
-
-
+    save_dicts_to_files(dicts, "textrank")
