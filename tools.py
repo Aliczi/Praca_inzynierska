b4635338de970bf766a6dbb82c6fbeaa2a9eaddf
panka134 <134panka@gmail.com>
2022-12-04 22:08:29 +0100

    Split to files and clean

---
+++ tools.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from datasets import load_dataset
+
+
+def save_dicts_to_files(dicts: dict, prefix: str):
+    for sentiment in dicts:
+        filename = f"{prefix}_{sentiment}.txt"
+        with open(filename, "w") as file:
+            file.write("\n".join(dicts[sentiment]))
+        print(f"Keywords for sentiment {sentiment} written to file {filename}")
+
+
+def load_data(data_path: str, data_category: str):
+    polemo_official = load_dataset(data_path, data_category)
+    df_polemo_official = pd.DataFrame(polemo_official["train"])
+
+    return df_polemo_official
