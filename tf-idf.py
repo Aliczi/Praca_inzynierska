26f0ecf99d2841c7eeb26c20617f41a1e42f2a66
panka134 <134panka@gmail.com>
2022-12-13 13:54:13 +0100

    Fix tf-idf

--- tf-idf.py
+++ tf-idf.py
@@ -1,21 +1,27 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
-from tools import load_data, save_dicts_to_files
+from tools import *
 
-n_keywords = 50
-df = load_data("data/polemo2-official/", "hotels_text")
+if __name__ == '__main__':
+    n_keywords = 100
+    polemo_category = "hotels_text"
+    # df = load_raw_data("data/polemo2-official/", polemo_category)
+    df = load_preprocessed_data(polemo_category)
 
-vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer()
 
-grouped = df.groupby("target")
-keywords = dict()
-for target in df["target"].unique():
-    targeted = grouped.get_group(target)["text"].values
-    tfidf_vector = vectorizer.fit_transform(targeted)
-    tf_idf_results = pd.DataFrame(
-        zip(*[vectorizer.get_feature_names_out(), vectorizer.idf_]),
-        columns=["word", "tf-idf"]
-        ).sort_values("tf-idf",  ascending=False)
-    keywords[target] = tf_idf_results["word"][:n_keywords]
+    grouped = df.groupby("target")
+    keywords = dict()
+    for target in df["target"].unique():
+        targeted = grouped.get_group(target)["text"].values
+        tfidf_vector = vectorizer.fit_transform(targeted)
+        tf_idf_results = pd.DataFrame(
+            zip(*[vectorizer.get_feature_names_out(), vectorizer.idf_]),
+            columns=["word", "tf-idf"]
+            ).sort_values("tf-idf",  ascending=False)
+        keywords[target] = list(tf_idf_results["word"][:n_keywords])
 
-save_dicts_to_files(keywords, "tfidf")
+    remove_word_from_dicts(keywords, "hotel")
+    remove_shared_words(keywords)
+
+    save_dicts_to_files(keywords, "tidf")
