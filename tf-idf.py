e9d76ebd27e28521e8d19d5522bf4da9acaeeb52
panka134 <134panka@gmail.com>
2023-01-27 02:09:01 +0100

    Fix tf-idf

--- tf-idf.py
+++ tf-idf.py
@@ -9,22 +9,33 @@ def tfidf(df: pd.DataFrame, n_keywords: int):
     keywords = dict()
     for target in df["target"].unique():
         targeted = grouped.get_group(target)["text"].values
-        vectorizer.fit_transform(targeted)
-        tf_idf_results = pd.DataFrame(
-            zip(*[vectorizer.get_feature_names_out(), vectorizer.idf_]),
-            columns=["word", "tf-idf"]
-            ).sort_values("tf-idf",  ascending=False)
-        keywords[target] = list(tf_idf_results["word"][:n_keywords])
+        tfidf_matrix = vectorizer.fit_transform(targeted)
+        tfidf_matrix_dataFrame = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix).mean(axis = 0).to_frame()
+        tfidf_matrix_dataFrame['id'] = tfidf_matrix_dataFrame.index
+        word_id = pd.DataFrame(vectorizer.vocabulary_.items(), columns = ["word", "id"])
+        
+        answer = pd.merge(tfidf_matrix_dataFrame, word_id, how="inner", on=["id"])
+
+        answer = answer.drop(columns=['id'], axis=1) \
+            .rename(columns={0: "mean_tf_idf"}) \
+            .sort_values("mean_tf_idf",  ascending=False) 
+
+        # df = pd.concat([tfidf_matrix_dataFrame, df4], axis=1).
+        # tf_idf_results = pd.DataFrame(
+        #     zip(*[vectorizer.get_feature_names_out(), vectorizer.idf_]),
+        #     columns=["word", "tf-idf"]
+        #     ).sort_values("tf-idf",  ascending=False)
+        keywords[target] = list(answer["word"][:n_keywords])
     return keywords
 
 
 if __name__ == '__main__':
     n_keywords = 100
     polemo_category = "hotels_text"
-    # df = load_raw_data("data/polemo2-official/", polemo_category)
-    df = load_preprocessed_data(polemo_category)
+    df = load_raw_data("data/polemo2-official/", polemo_category)
+    # df = load_preprocessed_data(polemo_category)
     keywords = tfidf(df, n_keywords)
-    remove_word_from_dicts(keywords, "hotel")
-    remove_shared_words(keywords)
+    # remove_word_from_dicts(keywords, "hotel")
+    # remove_shared_words(keywords)
 
-    save_dicts_to_files(keywords, "tidf")
+    save_dicts_to_files(keywords, "tfidf")
